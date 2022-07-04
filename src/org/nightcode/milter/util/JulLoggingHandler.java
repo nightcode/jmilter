f9c849fdcb4c5ee31cd7c11c14b98bbd4f0e38bc
dmitry <dmitry@nightcode.org>
2022-07-04 09:31:25 +0300

    Added implementation of LoggingHandler for JUL.

---
+++ src/org/nightcode/milter/util/JulLoggingHandler.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.nightcode.milter.util;
+
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public enum JulLoggingHandler implements Log.LoggingHandler {
+
+  DEBUG() {
+    @Override public void log(@NotNull Class<?> clazz, Supplier<String> supplier, @Nullable Throwable thrown) {
+      Logger.getLogger(clazz.getName()).log(Level.FINER, thrown, supplier);
+    }
+  },
+  INFO() {
+    @Override public void log(@NotNull Class<?> clazz, Supplier<String> supplier, @Nullable Throwable thrown) {
+      Logger.getLogger(clazz.getName()).log(Level.INFO, thrown, supplier); 
+    }
+  },
+  WARN() {
+    @Override public void log(@NotNull Class<?> clazz, Supplier<String> supplier, @Nullable Throwable thrown) {
+      Logger.getLogger(clazz.getName()).log(Level.WARNING, thrown, supplier);
+    }
+  },
+  ERROR() {
+    @Override public void log(@NotNull Class<?> clazz, Supplier<String> supplier, @Nullable Throwable thrown) {
+      Logger.getLogger(clazz.getName()).log(Level.WARNING, thrown, supplier);
+    }
+  },
+  FATAL() {
+    @Override public void log(@NotNull Class<?> clazz, Supplier<String> supplier, @Nullable Throwable thrown) {
+      Logger.getLogger(clazz.getName()).log(Level.SEVERE, thrown, supplier);
+    }
+  }
+}
